ee87a6237a66f3231ce6ba268ad2eeeb2a0aed9b
e1b22066 <e1b22066@oit.ac.jp>
2024-11-01 17:17:00 +0900

    Player2の色を変更しました

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -24,69 +24,41 @@ public class SceneChangeOnClick : MonoBehaviour
         // ボタンクリック時にOnButtonClickメソッドを呼び出す
         myButton.onClick.AddListener(OnButtonClick);
 
-        m_joycons = JoyconManager.Instance.j;
+        /*m_joycons = JoyconManager.Instance.j;
 
         if (m_joycons == null || m_joycons.Count <= 0) return;
 
         m_joyconL = m_joycons.Find(c => c.isLeft);
-        m_joyconR = m_joycons.Find(c => !c.isLeft);
+        m_joyconR = m_joycons.Find(c => !c.isLeft);*/
     }
 
     private void Update()
     {
-        JoyControll();
-        PushJoyCon();
+        //PushJoyCon();
     }
 
-    private void PushJoyCon()
+    /*private void PushJoyCon()
     {
         if (m_joyconL.GetButton(Joycon.Button.DPAD_RIGHT) || m_joyconL.GetButton(Joycon.Button.DPAD_DOWN) || m_joyconL.GetButton(Joycon.Button.SHOULDER_1) 
             || m_joyconL.GetButton(Joycon.Button.SHOULDER_2) || m_joyconL.GetButton(Joycon.Button.DPAD_LEFT) || m_joyconL.GetButton(Joycon.Button.DPAD_UP) || m_joyconL.GetButton(Joycon.Button.STICK))
         {
+            Resources.UnloadUnusedAssets();
             SceneManager.LoadScene(sceneName);
         }
         else if(m_joyconR.GetButton(Joycon.Button.DPAD_RIGHT) || m_joyconR.GetButton(Joycon.Button.DPAD_DOWN) || m_joyconR.GetButton(Joycon.Button.SHOULDER_1)
             || m_joyconR.GetButton(Joycon.Button.SHOULDER_2) || m_joyconR.GetButton(Joycon.Button.DPAD_LEFT) || m_joyconR.GetButton(Joycon.Button.DPAD_UP) || m_joyconR.GetButton(Joycon.Button.STICK))
         {
+            Resources.UnloadUnusedAssets();
             SceneManager.LoadScene(sceneName);
         }
 
         
-    }
+    }*/
     private void OnButtonClick()
     {
         // シーンをロードする
+        Resources.UnloadUnusedAssets();
         SceneManager.LoadScene(sceneName);
     }
-
-    private void JoyControll()
-    {
-        m_pressedButtonL = null;
-        m_pressedButtonR = null;
-
-        if (m_joycons == null || m_joycons.Count <= 0) return;
-
-        foreach (var button in m_buttons)
-        {
-            if (m_joyconL.GetButton(button))
-            {
-                m_pressedButtonL = button;
-            }
-            if (m_joyconR.GetButton(button))
-            {
-                m_pressedButtonR = button;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            m_joyconL.SetRumble(160, 320, 0.6f, 200);
-        }
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            m_joyconR.SetRumble(160, 320, 0.6f, 200);
-        }
-    }
-
 
 }
