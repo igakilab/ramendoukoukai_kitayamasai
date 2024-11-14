f39bd5655c6d8134d8e5579650b2a8fefb8e5775
e1b22066 <e1b22066@oit.ac.jp>
2024-11-14 15:42:32 +0900

    ゲーム最終版

--- Assets/Scripts/left_PlayerManager.cs
+++ Assets/Scripts/left_PlayerManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using DG.Tweening;
 using System;
 using UnityEngine.SceneManagement;
-using System.Security.Cryptography;      //シーンの切り替えに必要
+using System.Security.Cryptography;
 
 public class left_PlayerManager : MonoBehaviour
 {
@@ -18,44 +18,44 @@ public class left_PlayerManager : MonoBehaviour
     private static readonly Joycon.Button[] m_buttons =
         Enum.GetValues(typeof(Joycon.Button)) as Joycon.Button[];
 
-    public float moveSpeed = 3f; // 移動速度
-    public AudioSource seAudioSource;
-    public float attackRadius; // 攻撃範囲
-    public Transform highShotPoint; // 高い位置のショットポイント
-    public Transform lowShotPoint; // 低い位置のショットポイント
-    public Transform CriticalShotPoint; // 低い位置のショットポイント
-    public Transform shotPoint; // ショットポイント
-    public GameObject deathEffectPrefab; // 死亡エフェクトのプレハブ
-    public GameObject GuardObject; // ガードオブジェクト
-    public GameObject bulletPrefab; // 弾のプレハブ
-    public GameObject CriticalbulletPrefab; // 弾のプレハブ
+    public AudioSource[] audioSources;
+    public float moveSpeed = 3f;
+    public float attackRadius;
+    public Transform highShotPoint;
+    public Transform lowShotPoint;
+    public Transform CriticalShotPoint;
+    public Transform shotPoint;
+    public GameObject deathEffectPrefab;
+    public GameObject GuardObject;
+    public GameObject bulletPrefab;
+    public GameObject CriticalbulletPrefab;
     public GameObject[] stockObject;
-    public float jumpForce = 5f; // ジャンプ力
-    public float jump_cnt = 0; // ジャンプ回数
-    public bool isRight; // 右向きかどうか
-    public float hp = 10; // ヒットポイント
+    public float jumpForce = 5f;
+    public float jump_cnt = 0;
+    public bool isRight;
+    public float hp = 10;
     public int CriticalPoint = 0;
     public int i = 0;
     public int cnt = 0;
-    public float coolTime = 0.3f; //待機時間
-    public float leftCoolTime; //待機している時間
-    public float forceAmount = 5f; // ふっとばす力の強さ
-    public Vector2 direction; // ふっとばす方向
-    public int stock = 3; // キャラクターの残機（ストック）
-    public Vector3 respawnPosition; // リスポーンする場所
-    public float respawnDelay = 3f; // リスポーンするまでの待機時間
+    public float coolTime = 0.3f;
+    public float leftCoolTime;
+    public float forceAmount = 5f;
+    public Vector2 direction;
+    public int stock = 3;
+    public Vector3 respawnPosition;
+    public float respawnDelay = 3f;
     public bool action_dead = false;
     public bool action_guard = false;
-    // 元の色を保存するための変数
-    private Color originalColor;
+    public bool action_c_1 = false;
+    public bool action_c_2 = false;
+    public bool action_c_3 = false;
 
-    // 変更する色
+
+    private Color originalColor;
     public Color newColor = Color.red;
 
     private Renderer renderer;
 
-
-    // HpGauge関連のフィールド
     [SerializeField] private Image healthImage;
     [SerializeField] private Image burnImage;
     [SerializeField] private Image CriticalImage;
@@ -67,16 +67,15 @@ public class left_PlayerManager : MonoBehaviour
     Rigidbody2D rb;
     Animator animator;
 
-    public string SceneName;    //読み込むシーン名
+    public string SceneName;
 
 
     void Start()
     {
-        
-        rb = GetComponent<Rigidbody2D>(); // Rigidbody2Dを取得
-        animator = GetComponent<Animator>(); // Animatorを取得
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         renderer = GetComponent<Renderer>();
-        HealthSetGauge(1f); // HpGaugeの初期化
+        HealthSetGauge(1f);
         CriticalSetGauge(0f);
 
         m_joycons = JoyconManager.Instance.j;
@@ -86,19 +85,39 @@ public class left_PlayerManager : MonoBehaviour
         m_joyconL = m_joycons.Find(c => c.isLeft);
         m_joyconR = m_joycons.Find(c => !c.isLeft);
 
-        respawnPosition  = transform.position;
+        respawnPosition = transform.position;
         originalColor = renderer.material.color;
+
+        // シーンアンロード時のリソース解放のためのイベントリスナーを追加
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
 
     private void Update()
     {
-        Movement(); // 移動処理
-        if(!action_dead)
+        if (m_joyconL.GetButton(Joycon.Button.DPAD_UP))
         {
-            Jump(); // ジャンプ処理
+            action_c_1 = true;
+        }
+        if (m_joyconL.GetButton(Joycon.Button.DPAD_LEFT))
+        {
+            action_c_2 = true;
+        }
+        if (m_joyconL.GetButton(Joycon.Button.DPAD_UP) && m_joyconL.GetButton(Joycon.Button.DPAD_LEFT))
+        {
+            action_c_3 = true;
+        }
+
+        if (m_joyconL.GetButton(Joycon.Button.SR) && m_joyconL.GetButton(Joycon.Button.SL) && action_c_1 == true && action_c_2 == true && action_c_3 == true)
+        {
+            GuardClear(0.2f);
+        }
+            Movement();
+        if (!action_dead)
+        {
+            Jump();
             action_guard = false;
-            Guard(); // ガード処理
-            if(!action_guard)
+            Guard();
+            if (!action_guard)
             {
                 Shot();
                 if (CriticalPoint >= 5)
@@ -106,35 +125,33 @@ public class left_PlayerManager : MonoBehaviour
                     CriticalShot();
                 }
             }
-            
         }
         cnt = 0;
     }
-
 
     void Movement()
     {
         float x = 0f;
         if (m_joyconL != null)
         {
-            x += m_joyconL.GetStick()[0]; 
+            x += m_joyconL.GetStick()[0];
         }
 
         if (!isRight && x > 0)
         {
-            transform.Rotate(0f, 180f, 0f); 
+            transform.Rotate(0f, 180f, 0f);
             isRight = true;
         }
         if (isRight && x < 0)
         {
-            transform.Rotate(0f, 180f, 0f); 
+            transform.Rotate(0f, 180f, 0f);
             isRight = false;
         }
         if (!action_dead)
         {
-            animator.SetFloat("Speed", Mathf.Abs(x)); 
+            animator.SetFloat("Speed", Mathf.Abs(x));
         }
-        rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y); 
+        rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
     }
 
     void Shot()
@@ -143,57 +160,56 @@ public class left_PlayerManager : MonoBehaviour
         float flag = 0;
         if (m_joyconL.GetButton(Joycon.Button.DPAD_RIGHT))
         {
-            shotPoint = highShotPoint; 
+            shotPoint = highShotPoint;
             flag = 1;
         }
         if (m_joyconL.GetButton(Joycon.Button.DPAD_DOWN))
         {
-            shotPoint = lowShotPoint; 
+            shotPoint = lowShotPoint;
             flag = 1;
         }
         if (flag == 1 && leftCoolTime <= 0)
         {
-            PlaySound();
-            animator.SetTrigger("attack"); 
-            Instantiate(bulletPrefab, shotPoint.position, transform.rotation); 
+            PlaySound(0);
+            animator.SetTrigger("attack");
+            Instantiate(bulletPrefab, shotPoint.position, transform.rotation);
             leftCoolTime = coolTime;
         }
     }
 
     void CriticalShot()
     {
-        if (m_joyconL.GetAccel()[0] > 2)
+        if (m_joyconL.GetAccel()[2] > 2)
         {
-            animator.SetTrigger("attack"); 
-            int j;
-            for(j = 0; j < 10; j++)
-            {
-                Instantiate(CriticalbulletPrefab, CriticalShotPoint.position, transform.rotation); 
-            }
+            PlaySound(4);
+            animator.SetTrigger("attack");
+            Instantiate(CriticalbulletPrefab, CriticalShotPoint.position, transform.rotation);
             leftCoolTime = coolTime;
             CriticalPoint = 0;
             CriticalSetGauge(0f);
+            action_c_1 = false;
+            action_c_2 = false;
+            action_c_3 = false;
         }
     }
 
     void Jump()
     {
         Vector3 pos = rb.transform.position;
-        if (jump_cnt < 2)
-        {
+        //if (jump_cnt < 2)
+        //{
             if (m_joyconL.GetButton(Joycon.Button.STICK))
             {
-                
-                    animator.SetTrigger("jump"); 
-                    rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); 
-                    jump_cnt++;
-                
+                PlaySound(5);
+                animator.SetTrigger("jump");
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                jump_cnt++;
             }
-        }
-        if (-2.65f < pos.y && pos.y < -2.50f)
+        //}
+        /*if (-2.65f < pos.y && pos.y < -2.50f)
         {
-            jump_cnt = 0; 
-        }
+            jump_cnt = 0;
+        }*/
     }
 
     void Guard()
@@ -202,60 +218,68 @@ public class left_PlayerManager : MonoBehaviour
 
         if (m_joyconL.GetButton(Joycon.Button.SHOULDER_1))
         {
-            shotPoint = highShotPoint; 
+            shotPoint = highShotPoint;
             flag = 1;
         }
         if (m_joyconL.GetButton(Joycon.Button.SHOULDER_2))
         {
-            shotPoint = lowShotPoint; 
+            shotPoint = lowShotPoint;
             flag = 1;
         }
 
         if (flag == 1)
         {
             action_guard = true;
-            animator.SetTrigger("guard"); 
-            Instantiate(GuardObject, shotPoint.position, transform.rotation); 
+            animator.SetTrigger("guard");
+            Instantiate(GuardObject, shotPoint.position, transform.rotation);
         }
     }
 
-    public void OnDamage()
+    public void OnDamage(float damage)
+{
+    hp -= damage;
+    TakeDamage(0.1f);
+    ChangeColor();
+    Invoke("ResetColor", 0.1f);
+    PlaySound(2);
+    if (hp <= 0)
     {
-        
-        hp -= 1; 
-        TakeDamage(0.1f); 
-        ChangeColor();
-        Invoke("ResetColor", 0.1f);
-        if (hp <= 0)
+        stock--;
+
+        if (stock > 0)
         {
-            stock--; 
+            PlaySound(1);
+            hp = 10f;
+            CriticalPoint = 0;
+            HealthSetGauge(1f);
+            CriticalSetGauge(0f);
 
-            if (stock > 0)
-            {
-                hp = 10f;
-                CriticalPoint = 0;
-                HealthSetGauge(1f); 
-                CriticalSetGauge(0f);
-     
-                Destroy(stockObject[i]);
-                i++;
-                rb.velocity = Vector3.zero;
-                StartCoroutine(Respawn(rb));
-
+            Destroy(stockObject[i]);
+            i++;
+            rb.velocity = Vector3.zero;
+            StartCoroutine(Respawn(rb));
+                
             }
-            else
-            {
-                Instantiate(deathEffectPrefab, transform.position, transform.rotation); 
-                Destroy(gameObject);
+        else
+        {
+            Instantiate(deathEffectPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+                //PlaySound(6);
+                //Invoke("DelayedLoad", 2f); // 3秒後にDelayedLoadメソッドを呼び出す
                 Load();
-            }
         }
     }
+}
 
-    // HpGauge関連のメソッド
+private void DelayedLoad()
+{
+        
+        Load();
+}
+
+
     public void HealthSetGauge(float value)
     {
-        //DoTweenを連結して動かす
         healthImage.DOFillAmount(value, duration)
             .OnComplete(() =>
             {
@@ -263,15 +287,10 @@ public class left_PlayerManager : MonoBehaviour
                     .DOFillAmount(value, duration / 2f)
                     .SetDelay(0.5f);
             });
-        //transform.DOShakePosition(
-        //    duration / 2f,
-        //   strength, vibrate);
 
-        // シェーダーの確認
         Material material = healthImage.material;
         Debug.Log("使用しているシェーダー: " + material.shader.name);
 
-        // マテリアルのプロパティ確認
         Debug.Log("マテリアルのプロパティ: " + material.GetTexture("_MainTex"));
 
         healthcurrentRate = value;
@@ -280,7 +299,7 @@ public class left_PlayerManager : MonoBehaviour
     public void CriticalSetGauge(float value)
     {
         CriticalImage.DOFillAmount(value, duration);
-        
+
         Material material = CriticalImage.material;
         Debug.Log("使用しているシェーダー: " + material.shader.name);
 
@@ -296,6 +315,7 @@ public class left_PlayerManager : MonoBehaviour
 
     public void GuardClear(float rate)
     {
+        PlaySound(3);
         CriticalPoint++;
         CriticalSetGauge(criticalcurrentRate + rate);
     }
@@ -303,9 +323,8 @@ public class left_PlayerManager : MonoBehaviour
     private IEnumerator Respawn(Rigidbody2D rb)
     {
         Debug.Log("リスポーン待機中...");
-        
+
         Physics2D.gravity = Vector2.zero;
-        // 数秒間待機
         action_dead = true;
         transform.position = respawnPosition;
         yield return new WaitForSeconds(respawnDelay);
@@ -315,10 +334,20 @@ public class left_PlayerManager : MonoBehaviour
         Debug.Log("リスポーン完了");
     }
 
-    //シーンを読み込む
     public void Load()
     {
         SceneManager.LoadScene(SceneName);
+    }
+
+    private void OnSceneUnloaded(Scene current)
+    {
+        DOTween.KillAll();
+        Resources.UnloadUnusedAssets();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
     }
 
     //接触開始
@@ -327,13 +356,13 @@ public class left_PlayerManager : MonoBehaviour
         if(collision.gameObject.tag == "Dead")
         {
             hp = 1;
-            OnDamage();
+            OnDamage(1f);
         }
     }
 
-    public void PlaySound()
+    public void PlaySound(int n)
     {
-        seAudioSource.Play();
+        audioSources[n].Play();
     }
 
     public void ChangeColor()
--- Assets/Scripts/right_PlayerManager.cs
+++ Assets/Scripts/right_PlayerManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
-using UnityEngine.SceneManagement;      //シーンの切り替えに必要
-
+using UnityEngine.SceneManagement;
 
 public class right_PlayerManager : MonoBehaviour
 {
@@ -14,35 +13,37 @@ public class right_PlayerManager : MonoBehaviour
     private Joycon.Button? m_pressedButtonL;
     private Joycon.Button? m_pressedButtonR;
 
-
-    public float moveSpeed = 3f; // 移動速度
-    public AudioSource seAudioSource;
-    public float attackRadius; // 攻撃範囲
-    public Transform highShotPoint; // 高い位置のショットポイント
-    public Transform lowShotPoint; // 低い位置のショットポイント
-    public Transform CriticalShotPoint; // 低い位置のショットポイント
-    public Transform shotPoint; // ショットポイント
-    public GameObject deathEffectPrefab; // 死亡エフェクトのプレハブ
-    public GameObject GuardObject; // ガードオブジェクト
-    public GameObject bulletPrefab; // 弾のプレハブ
-    public GameObject CriticalbulletPrefab; // 弾のプレハブ
+    public AudioSource[] audioSources;
+    public float moveSpeed = 3f;
+    public float attackRadius;
+    public Transform highShotPoint;
+    public Transform lowShotPoint;
+    public Transform CriticalShotPoint;
+    public Transform shotPoint;
+    public GameObject deathEffectPrefab;
+    public GameObject GuardObject;
+    public GameObject bulletPrefab;
+    public GameObject CriticalbulletPrefab;
     public GameObject[] stockObject;
-    public float jumpForce = 5f; // ジャンプ力
-    public float jump_cnt = 0; // ジャンプ回数
-    public bool isRight; // 右向きかどうか
-    public float hp = 10; // ヒットポイント
+    public float jumpForce = 5f;
+    public float jump_cnt = 0;
+    public bool isRight;
+    public float hp = 10;
     public int CriticalPoint = 0;
     public int i = 0;
-    public float coolTime = 0.3f; //待機時間
-    public float leftCoolTime; //待機している時間
-    public float forceAmount = 1f; // ふっとばす力の強さ
-    public Vector2 direction; // ふっとばす方向
-    public int stock = 3; // キャラクターの残機（ストック）
-    public Vector3 respawnPosition; // リスポーンする場所
-    public float respawnDelay = 3f; // リスポーンするまでの待機時間
+    public float coolTime = 0.3f;
+    public float leftCoolTime;
+    public float forceAmount = 1f;
+    public Vector2 direction;
+    public int stock = 3;
+    public Vector3 respawnPosition;
+    public float respawnDelay = 3f;
     public bool action_dead = false;
     public bool action_guard = false;
-    // HpGauge関連のフィールド
+    public bool action_c_1 = false;
+    public bool action_c_2 = false;
+    public bool action_c_3 = false;
+
     [SerializeField] private Image healthImage;
     [SerializeField] private Image burnImage;
     [SerializeField] private Image CriticalImage;
@@ -51,27 +52,23 @@ public class right_PlayerManager : MonoBehaviour
     public int vibrate = 100;
     private float healthcurrentRate = 1f;
     private float criticalcurrentRate = 1f;
-    // 元の色を保存するための変数
-    private Color originalColor;
 
-    // 変更する色
+    private Color originalColor;
     public Color newColor = Color.red;
 
     private Renderer renderer;
-
     Rigidbody2D rb;
     Animator animator;
 
-    public string SceneName;    //読み込むシーン名
+    public string SceneName;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Rigidbody2Dを取得
-        animator = GetComponent<Animator>(); // Animatorを取得
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         renderer = GetComponent<Renderer>();
-        HealthSetGauge(1f); // HpGaugeの初期化
+        HealthSetGauge(1f);
         CriticalSetGauge(0f);
-
 
         m_joycons = JoyconManager.Instance.j;
 
@@ -82,17 +79,38 @@ public class right_PlayerManager : MonoBehaviour
 
         respawnPosition = transform.position;
         originalColor = renderer.material.color;
+
+        // シーン切り替え前にメモリをクリアするためのイベントリスナーを追加
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
 
     private void Update()
     {
-        //JoyControll();
-        Movement(); // 移動処理
+        
+
+        if (m_joyconR.GetButton(Joycon.Button.DPAD_UP))
+        {
+            action_c_1 = true;
+        }
+        if (m_joyconR.GetButton(Joycon.Button.DPAD_LEFT))
+        {
+            action_c_2 = true;
+        }
+        if (m_joyconR.GetButton(Joycon.Button.DPAD_UP) && m_joyconR.GetButton(Joycon.Button.DPAD_LEFT))
+        {
+            action_c_3 = true;
+        }
+
+        if (m_joyconR.GetButton(Joycon.Button.SR) && m_joyconR.GetButton(Joycon.Button.SL) && action_c_1 == true && action_c_2 == true && action_c_3 == true)
+        {
+            GuardClear(0.2f);
+        }
+        Movement();
         if (!action_dead)
         {
-            Jump(); // ジャンプ処理
+            Jump();
             action_guard = false;
-            Guard(); // ガード処理
+            Guard();
             if (!action_guard)
             {
                 Shot();
@@ -109,25 +127,25 @@ public class right_PlayerManager : MonoBehaviour
         float x = 0f;
         if (m_joyconL != null)
         {
-            x += m_joyconR.GetStick()[0]; // 右Joy-Conの横軸入力を取得
+            x += m_joyconR.GetStick()[0];
         }
 
         if (!isRight && x > 0)
         {
-            transform.Rotate(0f, 180f, 0f); // 右向きに回転
+            transform.Rotate(0f, 180f, 0f);
             isRight = true;
         }
         if (isRight && x < 0)
         {
-            transform.Rotate(0f, 180f, 0f); // 左向きに回転
+            transform.Rotate(0f, 180f, 0f);
             isRight = false;
         }
 
         if (!action_dead)
         {
-            animator.SetFloat("Speed", Mathf.Abs(x)); // アニメーションの速度を設定
+            animator.SetFloat("Speed", Mathf.Abs(x));
         }
-        rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y); // 移動速度を設定
+        rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
     }
 
     void Shot()
@@ -136,57 +154,61 @@ public class right_PlayerManager : MonoBehaviour
         float flag = 0;
         if (m_joyconR.GetButton(Joycon.Button.DPAD_RIGHT))
         {
-            shotPoint = highShotPoint; // 高い位置からショット
+            shotPoint = highShotPoint;
             flag = 1;
         }
         if (m_joyconR.GetButton(Joycon.Button.DPAD_DOWN))
         {
-            shotPoint = lowShotPoint; // 低い位置からショット
+            shotPoint = lowShotPoint;
             flag = 1;
         }
         if (flag == 1 && leftCoolTime <= 0)
         {
-            animator.SetTrigger("attack"); // 攻撃アニメーションを再生
-            seAudioSource.Play();
-            Instantiate(bulletPrefab, shotPoint.position, transform.rotation); // 弾を生成
+            animator.SetTrigger("attack");
+            PlaySound(0);
+            Instantiate(bulletPrefab, shotPoint.position, transform.rotation);
             leftCoolTime = coolTime;
         }
     }
 
     void CriticalShot()
     {
-        if (m_joyconR.GetAccel()[0] > 2)
+        if (m_joyconR.GetAccel()[2] > 2)
         {
-            animator.SetTrigger("attack"); // 攻撃アニメーションを再生
-            int j;
-            for (j = 0; j < 10; j++)
-            {
-                Instantiate(CriticalbulletPrefab, CriticalShotPoint.position, transform.rotation); // 弾を生成
-            }
+            PlaySound(4);
+            animator.SetTrigger("attack");
+            Instantiate(CriticalbulletPrefab, CriticalShotPoint.position, transform.rotation);
             leftCoolTime = coolTime;
             CriticalPoint = 0;
             CriticalSetGauge(0f);
+            action_c_1 = false;
+            action_c_2 = false;
+            action_c_3 = false;
         }
     }
 
     void Jump()
     {
         Vector3 pos = rb.transform.position;
-        if (jump_cnt < 2)
-        {
+        //if (jump_cnt < 2)
+        //{
             if (m_joyconR.GetButton(Joycon.Button.STICK))
             {
-               
-                    animator.SetTrigger("jump"); // ジャンプアニメーションを再生
-                    rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // ジャンプ力を加える
-                    jump_cnt++;
-                
+                PlaySound(5);
+                animator.SetTrigger("jump");
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                jump_cnt++;
             }
-        }
-        if (-2.65f < pos.y && pos.y < -2.50f)
+        //}
+        /*if (collision.gameObject.tag == "")
         {
-            jump_cnt = 0; // ジャンプ回数をリセット
-        }
+            hp = 1;
+            OnDamage(1f);
+        }*/
+        /*if (-2.65f < pos.y && pos.y < -2.50f)
+        {
+            jump_cnt = 0;
+        }*/
     }
 
     void Guard()
@@ -195,60 +217,67 @@ public class right_PlayerManager : MonoBehaviour
 
         if (m_joyconR.GetButton(Joycon.Button.SHOULDER_1))
         {
-            shotPoint = highShotPoint; // 高い位置でガード
+            shotPoint = highShotPoint;
             flag = 1;
         }
         if (m_joyconR.GetButton(Joycon.Button.SHOULDER_2))
         {
-            shotPoint = lowShotPoint; // 低い位置でガード
+            shotPoint = lowShotPoint;
             flag = 1;
         }
 
         if (flag == 1)
         {
             action_guard = true;
-            animator.SetTrigger("guard"); // ガードアニメーションを再生
-            Instantiate(GuardObject, shotPoint.position, transform.rotation); // ガードオブジェクトを生成
+            animator.SetTrigger("guard");
+            Instantiate(GuardObject, shotPoint.position, transform.rotation);
         }
     }
 
-    public void OnDamage()
+    public void OnDamage(float damage)
     {
-        Debug.Log("ダメージを受けた"); // デバッグログを追加
-        hp -= 1; // ヒットポイントを減少
-        TakeDamage(0.1f); // HpGaugeの更新
-        rb.velocity = Vector3.zero;
+        hp -= damage;
+        TakeDamage(0.1f);
         ChangeColor();
         Invoke("ResetColor", 0.1f);
-        //Fling(new Vector2(1, 1));
+        PlaySound(2);
         if (hp <= 0)
         {
-            stock--; // 残機を減少
+            stock--;
 
             if (stock > 0)
             {
+                PlaySound(1);
                 hp = 10f;
                 CriticalPoint = 0;
-                HealthSetGauge(1f); // HpGaugeの初期化
+                HealthSetGauge(1f);
                 CriticalSetGauge(0f);
-                // プレイヤーをリスポーン位置に移動
+
                 Destroy(stockObject[i]);
                 i++;
+                rb.velocity = Vector3.zero;
                 StartCoroutine(Respawn(rb));
             }
             else
             {
-                Instantiate(deathEffectPrefab, transform.position, transform.rotation); // 死亡エフェクトを生成
-                Destroy(gameObject); // プレイヤーオブジェクトを破壊
+                Instantiate(deathEffectPrefab, transform.position, transform.rotation);
+                Destroy(gameObject);
                 Load();
+                //PlaySound(6);
+                //Invoke("DelayedLoad", 2f); // 3秒後にDelayedLoadメソッドを呼び出す
             }
         }
     }
 
-    // HpGauge関連のメソッド
+    private void DelayedLoad()
+    {
+        Destroy(gameObject);
+        Load();
+    }
+
+
     public void HealthSetGauge(float value)
     {
-        //DoTweenを連結して動かす
         healthImage.DOFillAmount(value, duration)
             .OnComplete(() =>
             {
@@ -259,12 +288,10 @@ public class right_PlayerManager : MonoBehaviour
 
         healthcurrentRate = value;
     }
-    
+
     public void CriticalSetGauge(float value)
     {
-        //DoTweenを連結して動かす
         CriticalImage.DOFillAmount(value, duration);
-
         criticalcurrentRate = value;
     }
 
@@ -275,20 +302,9 @@ public class right_PlayerManager : MonoBehaviour
 
     public void GuardClear(float rate)
     {
+        PlaySound(3);
         CriticalPoint++;
         CriticalSetGauge(criticalcurrentRate + rate);
-    }
-
-    public void Fling(Vector2 direction)
-    {
-        if (rb != null)
-        {
-            // 力を加える（2Dのワールド座標に基づく）
-            Debug.Log("吹っ飛ばす方向: " + direction);
-
-            animator.SetTrigger("addforce");
-            rb.AddForce(direction.normalized * 10f, ForceMode2D.Impulse);
-        }
     }
 
     private IEnumerator Respawn(Rigidbody2D rb)
@@ -296,7 +312,6 @@ public class right_PlayerManager : MonoBehaviour
         Debug.Log("リスポーン待機中...");
 
         Physics2D.gravity = Vector2.zero;
-        // 数秒間待機
         action_dead = true;
         transform.position = respawnPosition;
         yield return new WaitForSeconds(respawnDelay);
@@ -306,25 +321,36 @@ public class right_PlayerManager : MonoBehaviour
         Debug.Log("リスポーン完了");
     }
 
-    //シーンを読み込む
     public void Load()
     {
         SceneManager.LoadScene(SceneName);
     }
 
-    //接触開始
+    // シーンがアンロードされた時にオブジェクトを破棄してメモリを解放する
+    private void OnSceneUnloaded(Scene current)
+    {
+        // DOTweenのメモリをクリアする
+        DOTween.KillAll();
+        Resources.UnloadUnusedAssets();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Dead")
         {
             hp = 1;
-            OnDamage();
+            OnDamage(1f);
         }
     }
 
-    public void PlaySound()
+    public void PlaySound(int n)
     {
-        seAudioSource.Play();
+        audioSources[n].Play();
     }
 
     public void ChangeColor()
@@ -335,7 +361,6 @@ public class right_PlayerManager : MonoBehaviour
         }
     }
 
-    // 元の色に戻すメソッド
     public void ResetColor()
     {
         if (renderer != null)
